23de8d5d71637d54d89a69ae53c092c3a81b49bc
Lillie Schachter <lillieschachter@gmail.com>
2020-11-28 11:32:37 -0800

    Add scraper to display number of answers for the day's spelling bee'

---
+++ bee_scraper.py
@@ -0,0 +1,28 @@
+from bs4 import BeautifulSoup
+import requests
+
+BEE_SITE = 'https://www.nytimes.com/puzzles/spelling-bee'
+
+def scrape_answers(req_lib, scraper):
+    '''
+    A quick scraper solely to find the number of words available in the new york time's spelling bee
+    game. NOT designed to display the words, just to make the end goal clearer!
+    '''
+    response = req_lib.get(BEE_SITE)
+    scrape = scraper(response.text, 'html.parser')
+    # Isolate the data we need, kept in the first js script on the page
+    scrape = scrape.script.string
+
+    answer_key = '"answers":'
+    answers_index = scrape.index('"answers":')
+    answers_index += len(answer_key)
+    scrape = scrape[answers_index:]
+    list_end = scrape.index(']')
+    scrape = scrape[:(list_end - 1)]
+
+    words = scrape.split(',')
+    return len(words)
+
+
+if __name__ == "__main__":
+    print(f'There are {scrape_answers(requests, BeautifulSoup)} answers in today\'s Spelling Bee')
